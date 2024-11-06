63491b782890322e9135b2969799debd487f468b
Saralrat <saralrats@ecosoft.co.th>
2024-11-06 11:30:40 +0700

    Customer data can now be retrieved.

--- menghua_co/custom/sales_billing.py
+++ menghua_co/custom/sales_billing.py
@@ -3,18 +3,17 @@ from frappe import _
 from erpnext.accounts.doctype.payment_entry.payment_entry import get_payment_entry
 
 @frappe.whitelist()
-def create_payment_entry(sales_billing):
-    sales_billing = frappe.get_doc("Sales Billing", sales_billing)
-
-    if not sales_billing.customer:
-        frappe.throw(_("Customer is required to create a Payment Entry."))
-
+def create_payment_entry(sales_billing_name):
+    sales_billing = frappe.get_doc("Sales Billing", sales_billing_name)
+    
     payment_entry = frappe.new_doc("Payment Entry")
+    payment_entry.payment_type = "Receive"
     payment_entry.party_type = "Customer"
     payment_entry.party = sales_billing.customer
-    payment_entry.paid_amount = sales_billing.total_billing_amount
-    payment_entry.reference = sales_billing.name
-    payment_entry.payment_type = "Receive"
+    payment_entry.party_name = sales_billing.customer_name
+    payment_entry.paid_amount = sales_billing.total_billing_amount 
+    payment_entry.received_amount = sales_billing.total_billing_amount 
+    payment_entry.paid_to = sales_billing.total_billing_amount
 
     payment_entry.insert()
     return payment_entry.name
--- menghua_co/public/js/sales_billing.js
+++ menghua_co/public/js/sales_billing.js
@@ -1,15 +1,22 @@
 frappe.ui.form.on('Sales Billing', {
-    refresh(frm) {
-        if (frm.doc.docstatus === 1 && frm.doc.outstanding_amount !== 0) {
-            frm.add_custom_button(__('Payment'), function() {
-                frappe.call({
-                    method: "menghua_co.custom.sales_billing.create_payment_entry",
-                    args: {
-                        sales_billing: frm.doc.name  
-                    },
-                });
-            }, __("Action"));
-            frm.page.set_inner_btn_group_as_primary(__('Action'));
-        }
+    refresh: function(frm) {
+        frm.add_custom_button(__('Payment'), function() {
+            frm.events.create_payment_entry(frm);
+        }, __('Action'));
+        frm.page.set_inner_btn_group_as_primary(__('Action'));
+    },
+
+    create_payment_entry: function(frm) {
+        frappe.call({
+            method: 'menghua_co.custom.sales_billing.create_payment_entry',  
+            args: {
+                sales_billing_name: frm.doc.name
+            },
+            callback: function(r) {
+                if (r.message) {
+                    frappe.set_route('Form', 'Payment Entry', r.message);
+                }
+            }
+        });
     }
 });
