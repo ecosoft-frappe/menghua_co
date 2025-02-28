087e9aa1775fd7beb206c10d7e15894a44f5d401
newtratip <tharathip.chaweewongphan@gmail.com>
2025-02-28 15:45:26 +0700

    Add coating detail

--- menghua_co/menghua_co/doctype/manufacturing_order/manufacturing_order.js
+++ menghua_co/menghua_co/doctype/manufacturing_order/manufacturing_order.js
@@ -1,33 +1,17 @@
 // Copyright (c) 2024, Ecosoft and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Manufacturing Order', {
-    customer: function(frm) {
-        frm.set_query('sales_order', function() {
-            return {
-                filters: {
-                    'customer': frm.doc.customer
-                }
-            };
-        });
-    }
-});
-
-frappe.ui.form.on('Manufacturing Order', {
+frappe.ui.form.on("Manufacturing Order", {
     onload: function(frm) {
         if (frm.doc.docstatus == 0 && frm.doc.mh_status == "ยกเลิก") {
-            frm.set_value('mh_status', 'ร่าง');
+            frm.set_value("mh_status", "ร่าง");
         }
     },
-    
     validate: function(frm) {
         if (frm.doc.docstatus == 0 && frm.doc.mh_status == "ยกเลิก") {
-            frm.set_value('mh_status', 'ร่าง');
+            frm.set_value("mh_status", "ร่าง");
         }
-    }
-});
-
-frappe.ui.form.on("Manufacturing Order", {
+    },
     refresh: function (frm) {
         if (frm.doc.docstatus === 1) {
             frm.toggle_display("sales_team_section", true);
@@ -53,4 +37,16 @@ frappe.ui.form.on("Manufacturing Order", {
             frm.toggle_display("sales_team_section", true);
         }
     },
+    customer: function(frm) {
+        frm.set_query("sales_order", function() {
+            return {
+                filters: {
+                    "customer": frm.doc.customer
+                }
+            };
+        });
+    },
+    coating: function(frm) {
+        frm.set_value("coating_detail", "");
+    }
 });
